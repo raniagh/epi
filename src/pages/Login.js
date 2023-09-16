c9bf03e23aabac30d6320e0eb038f46c7131d42c
rania ghzaiel <rania.ghzaiel@zestyswiss.com>
2023-09-16 13:47:23 +0200

    fix part7

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,12 +7,12 @@ Portail étudiant SWISSWAI
 =========================================================
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-//import { ConfigB2C } from "configurations/ConfigurationB2C";
+import { ConfigB2C } from "configurations/ConfigurationB2C";
 import { useEffect } from "react";
 
 export const Login = () => {
   useEffect(() => {
-    /* var queryParams =
+    var queryParams =
       "client_id=" +
       ConfigB2C.client_id +
       "&nonce=anyRandomValue" +
@@ -21,8 +21,9 @@ export const Login = () => {
       "&scope=https://swisswaib2c.onmicrosoft.com/api/" +
       ConfigB2C.scope +
       "&response_type=code";
-    var fullTokenEndpoint = ConfigB2C.tokenEndpointAuthorize + queryParams; */
+    // var fullTokenEndpoint = ConfigB2C.tokenEndpointAuthorize + queryParams;
     window.location.href =
-      "https://swisswaib2c.b2clogin.com/swisswaib2c.onmicrosoft.com/B2C_1_SignIn/oauth2/v2.0/authorize?";
+      "https://swisswaib2c.b2clogin.com/swisswaib2c.onmicrosoft.com/B2C_1_SignIn/oauth2/v2.0/authorize?" +
+      queryParams;
   });
 };
